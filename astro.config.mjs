94280873e2049bed4f8050bff9cced4aa15b78b5
Gabriel Karczewski <gabriel.karczewski@lite.tech>
2023-07-02 23:38:16 +0200

    switch to native html minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 
-import frontendistahtmlMinify from "@frontendista/astro-html-minify";
-
-// https://astro.build/config
 export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
-  integrations: [frontendistahtmlMinify()],
+  compressHTML: true,
 });
