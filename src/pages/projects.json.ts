a91b06ac78c4996e20ed85d929a1489da5e86213
Gabriel Karczewski <nickelghost@outlook.com>
2023-12-28 23:30:37 +0100

    improve the api results

--- src/pages/projects.json.ts
+++ src/pages/projects.json.ts
@@ -3,7 +3,36 @@ import { getCollection } from "astro:content";
 export async function GET() {
   const projects = await getCollection("projects");
 
-  return new Response(JSON.stringify(projects.map((p) => p.data)), {
+  const processedProjects = projects
+    .sort((a, b) => b.data.priority - a.data.priority)
+    .map((p) => p.data)
+    .map((p) =>
+      p.image
+        ? {
+            ...p,
+            image: {
+              ...p.image,
+              src: "https://nickelghost.com" + p.image.src,
+            },
+          }
+        : p
+    )
+    .map((p) =>
+      p.name === "nickelghost.com"
+        ? {
+            ...p,
+            links: [
+              {
+                name: "Website",
+                url: "https://nickelghost.com",
+              },
+              ...p.links,
+            ],
+          }
+        : p
+    );
+
+  return new Response(JSON.stringify(processedProjects), {
     headers: { "content-type": "application/json" },
   });
 }
--- src/pages/workplaces.json.ts
+++ src/pages/workplaces.json.ts
@@ -3,7 +3,11 @@ import { getCollection } from "astro:content";
 export async function GET() {
   const workplaces = await getCollection("workplaces");
 
-  return new Response(JSON.stringify(workplaces.map((p) => p.data)), {
+  const processedWorkplaces = workplaces
+    .sort((a, b) => b.data.priority - a.data.priority)
+    .map((wp) => wp.data);
+
+  return new Response(JSON.stringify(processedWorkplaces), {
     headers: { "content-type": "application/json" },
   });
 }
