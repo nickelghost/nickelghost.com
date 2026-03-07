4b5a2ea744d931de1ef0ee1bfe7afc7fab118a8d
Gabriel Karczewski <nickelghost@outlook.com>
2026-03-07 19:17:32 +0100

    implement astro site url config

--- src/pages/projects.json.ts
+++ src/pages/projects.json.ts
@@ -1,30 +1,34 @@
+import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
 
-export async function GET() {
+export async function GET({ site }: APIContext) {
+  const origin = site?.origin ?? "";
   const projects = await getCollection("projects");
 
   const processedProjects = projects
     .sort((a, b) => b.data.priority - a.data.priority)
     .map((p) => p.data)
+    // add the site URL to the image src so it's resolvable outside the site
     .map((p) =>
       p.image
         ? {
             ...p,
             image: {
               ...p.image,
-              src: "https://nickelghost.com" + p.image.src,
+              src: origin + p.image.src,
             },
           }
         : p,
     )
+    // for the project of the actual site, add a link to the website as well
     .map((p) =>
-      p.name === "nickelghost.com"
+      origin && p.name === new URL(origin).hostname
         ? {
             ...p,
             links: [
               {
                 name: "Website",
-                url: "https://nickelghost.com",
+                url: origin,
               },
               ...p.links,
             ],
